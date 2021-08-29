afb60c9109d37d8b6debcf17fed6f6b15a8b8fd3
Crowdbotics <team@crowdbotics.com>
2021-08-29 15:34:31 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/shmn_wallet_30180/MainActivity.java
@@ -0,0 +1,15 @@
+package com.shmn_wallet_30180;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "shmn_wallet_30180";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "shmn_wallet_30180Identifier",
+  url: "https://shmn_wallet_30180Identifier.botics.co",
+  api: "https://shmn_wallet_30180Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
